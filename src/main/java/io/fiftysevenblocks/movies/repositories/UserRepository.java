dd14b23863eb62d0f510b2999b5a564a41b7e4f8
Cristian Martinez <cristianandres92@gmail.com>
2024-05-29 16:22:06 -0500

    Add user and movie repositories

---
+++ src/main/java/io/fiftysevenblocks/movies/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package io.fiftysevenblocks.movies.repositories;
+
+import io.fiftysevenblocks.movies.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
