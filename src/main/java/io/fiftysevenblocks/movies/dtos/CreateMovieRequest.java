162fb24dd614b36e407595656abae7e0ff1637ec
Cristian Martinez <cristianandres92@gmail.com>
2024-05-30 16:20:02 -0500

    Update create movie request constructor

--- src/main/java/io/fiftysevenblocks/movies/dtos/CreateMovieRequest.java
+++ src/main/java/io/fiftysevenblocks/movies/dtos/CreateMovieRequest.java
@@ -4,8 +4,4 @@ import java.time.LocalDate;
 
 public record CreateMovieRequest(String title, String description,
                                  LocalDate releaseDate, String genre, boolean isPrivate, Long userId) {
-
-    public CreateMovieRequest(String title, String description, LocalDate releaseDate, String genre, Long userId) {
-        this(title, description, releaseDate, genre, false, userId);
-    }
 }
