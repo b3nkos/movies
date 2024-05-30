dc6ea8a80d70d98db5fdf10c4478a508ebfd90a7
Cristian Martinez <cristianandres92@gmail.com>
2024-05-29 23:30:54 -0500

    Update email field as unique

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS movies
 (
-    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title       VARCHAR(255) NOT NULL,
-    description TEXT,
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title        VARCHAR(255) NOT NULL,
+    description  TEXT,
     release_date DATE,
-    genre       VARCHAR(100),
+    genre        VARCHAR(100),
     is_private   BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name       VARCHAR(255) NOT NULL,
-    lastname       VARCHAR(255) NOT NULL,
-    password       VARCHAR(255) NOT NULL,
-    email       VARCHAR(255) NOT NULL,
-    phone       VARCHAR(255) NOT NULL
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    phone    VARCHAR(255) NOT NULL
 );
