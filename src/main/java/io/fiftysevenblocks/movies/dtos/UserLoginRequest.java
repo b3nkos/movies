a0cbabef2c3dccd9f4354c8054a482622645ebfb
Cristian Martinez <cristianandres92@gmail.com>
2024-05-29 23:47:33 -0500

    Add validation annotations

--- src/main/java/io/fiftysevenblocks/movies/dtos/UserLoginRequest.java
+++ src/main/java/io/fiftysevenblocks/movies/dtos/UserLoginRequest.java
@@ -1,4 +1,26 @@
 package io.fiftysevenblocks.movies.dtos;
 
-public record UserLoginRequest(String email, String password) {
+import jakarta.validation.constraints.*;
+
+public record UserLoginRequest(
+        @NotBlank(
+                message = "Email must not be null or empty"
+        )
+        @Email(
+                message = "Email must be a well-formed email address"
+        )
+        String email,
+        @NotBlank(
+                message = "Password must not be null or empty"
+        )
+        @Size(
+                min = 10,
+                message = "Password must be at least 10 characters long"
+        )
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#?])[a-zA-Z!@#?]+$",
+                message = "Password must contain at least one lowercase letter, one uppercase letter, and one of the following characters: !, @, #, ? or ]"
+        )
+        String password
+) {
 }
--- src/main/java/io/fiftysevenblocks/movies/dtos/UserRegisterRequest.java
+++ src/main/java/io/fiftysevenblocks/movies/dtos/UserRegisterRequest.java
@@ -1,19 +1,38 @@
 package io.fiftysevenblocks.movies.dtos;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 
 public record UserRegisterRequest(
-        @NotBlank
+        @NotBlank(
+                message = "Name must not be null or empty"
+        )
         String name,
-        @NotBlank
+        @NotBlank(
+                message = "Last name must not be null or empty"
+        )
         String lastname,
-        @NotBlank
+        @NotBlank(
+                message = "Password must not be null or empty"
+        )
+        @Size(
+                min = 10,
+                message = "Password must be at least 10 characters long"
+        )
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#?])[a-zA-Z!@#?]+$",
+                message = "Password must contain at least one lowercase letter, one uppercase letter, and one of the following characters: !, @, #, ? or ]"
+        )
         String password,
-        @NotBlank
-        @Email
+        @NotBlank(
+                message = "Email must not be null or empty"
+        )
+        @Email(
+                message = "Email must be a well-formed email address"
+        )
         String email,
-        @NotBlank
+        @NotBlank(
+                message = "Phone must not be null or empty"
+        )
         String phone
 ) {
 }
