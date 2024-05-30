38dea3abe440db65d70aebe296fce586742b896c
Cristian Martinez <cristianandres92@gmail.com>
2024-05-30 16:16:53 -0500

    Add get movies repository method

--- src/main/java/io/fiftysevenblocks/movies/repositories/MovieRepository.java
+++ src/main/java/io/fiftysevenblocks/movies/repositories/MovieRepository.java
@@ -1,9 +1,12 @@
 package io.fiftysevenblocks.movies.repositories;
 
 import io.fiftysevenblocks.movies.models.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    Page<Movie> getAllByUserIdOrPublic(Long userId, boolean isPublic, Pageable pageable);
 }
