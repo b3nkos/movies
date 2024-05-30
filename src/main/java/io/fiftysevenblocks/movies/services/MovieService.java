ceea8e2ed06679a05bf0301bcf22552b19b4fc0c
Cristian Martinez <cristianandres92@gmail.com>
2024-05-30 16:17:08 -0500

    Add get movies service method

--- src/main/java/io/fiftysevenblocks/movies/services/MovieService.java
+++ src/main/java/io/fiftysevenblocks/movies/services/MovieService.java
@@ -1,9 +1,14 @@
 package io.fiftysevenblocks.movies.services;
 
 import io.fiftysevenblocks.movies.dtos.CreateMovieRequest;
+import io.fiftysevenblocks.movies.dtos.MovieResponse;
+import io.fiftysevenblocks.movies.dtos.MoviesRequest;
 import io.fiftysevenblocks.movies.mappers.MovieMapper;
 import io.fiftysevenblocks.movies.models.Movie;
 import io.fiftysevenblocks.movies.repositories.MovieRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,8 +21,18 @@ public class MovieService {
         this.movieMapper = movieMapper;
     }
 
-    public Movie save(CreateMovieRequest createMovieRequest) {
+    public MovieResponse save(CreateMovieRequest createMovieRequest) {
         Movie movie = movieMapper.fromMovieRequestToMovie(createMovieRequest);
-        return movieRepository.save(movie);
+        Movie savedMovie = movieRepository.save(movie);
+        return movieMapper.fromMovieToMovieResponse(savedMovie);
+    }
+
+    public Page<MovieResponse> getAllMovies(MoviesRequest moviesRequest) {
+        Pageable pageable = PageRequest.of(moviesRequest.page(), moviesRequest.size());
+
+        Page<Movie> moviePage = movieRepository.getAllByUserIdOrPublic(moviesRequest.userId(),
+                moviesRequest.isPrivate(), pageable);
+
+        return movieMapper.fromMoviePageToMovieResponsePage(moviePage);
     }
 }
